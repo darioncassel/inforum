be5c4870bbe43a43fa085af94f7ecf2b6236e666
raddar4 <darioncassel@Darions-MacBook-Pro.local>
2015-02-28 15:05:12 -0500

    Changes

--- Server/server.js
+++ Server/server.js
@@ -6,11 +6,20 @@ if (Meteor.isServer) {
   var authToken = '87884be75ea60edbb8fa1d0a8081020f';
   twilio = Twilio(accountSid, authToken);
 
-  var resp = new twilio.TwimlResponse();
-
-  var resp = new twilio.TwimlResponse();
-
-  resp.sms('Welcome to inforum!');
+  var express = Meteor.npmRequire('express');
+  var app = express();
+  app.post('/query', function(req, res) {
+    if (twilio.validateExpressRequest(req, authToken)) {
+        var twiml = new twilio.TwimlResponse();
+        twiml.sms('Hi!  Thanks for checking out my app!')
+        res.type('text/xml');
+        res.send(twiml.toString());
+    }
+    else {
+        res.send('you are not twilio. Buzz off.');
+    }
+  });
+  //app.listen(process.env.PORT || 3000);
 
   Meteor.methods({
     'sendSMS': function (number) {
@@ -26,59 +35,6 @@ if (Meteor.isServer) {
           console.log(err);
         }
       });
-    },
-    'updateDB': function () {
-      console.log('here');
-      /*
-      Fiber(function(){
-        twilio.messages.list(function(err, data) {
-          data.messages.forEach(function(message) {
-              MessageData.insert({from: message.from, body: message.body});
-          });
-        });
-      }).run();
-      */
     }
-  });
-/*
-  Router.route('/api/twiml/sms', 'POST', function() {
-    var rawIn = this.request.body;
-    if (Object.prototype.toString.call(rawIn) == "[object Object]") {
-        MessageData.insert(rawIn);
-    }
-
-    var question = {};
-    if (rawIn.Body) {
-        question.inputQuestion = rawIn.Body;
-        question.source = "sms";
-    } else if (rawIn.TranscriptionText) {
-        question.inputQuestion = rawIn.TranscriptionText;
-        question.source = "voicemail";
-    } else {
-        return;
-    }
-    question.inputName = rawIn.From;
-
-    var toOrig = rawIn.To;
-    toOrig = toOrig.replace(/\+1/g, "");
-    var toPretty = '('+toOrig.substr(0,3)+') '+toOrig.substr(3,3)+'-'+toOrig.substr(6,10);
-    var eventDetails = Events.findOne({phone: toPretty});
-
-    if (_.size(eventDetails) == 0) {
-        return;
-    } else {
-        question.slug = eventDetails.slug;
-    }
-
-    Meteor.call('questionCreate', question, function(error, res) {
-
-    });
-
-    var xml = '<Response><Sms>Thank you for submitting your question!</Sms></Response>';
-    return [200, {"Content-Type": "text/xml"}, xml];
-  });
-*/
-  Meteor.startup(function () {
-    //aksdklsja
   });
 }
