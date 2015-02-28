d03e0fe992de4d1a497f7cfc66330dd742d23163
raddar4 <darioncassel@Darions-MacBook-Pro.local>
2015-02-28 15:16:39 -0500

    There

--- Server/server.js
+++ Server/server.js
@@ -19,7 +19,7 @@ if (Meteor.isServer) {
         res.send('you are not twilio. Buzz off.');
     }
   });
-  //app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000);
 
   Meteor.methods({
     'sendSMS': function (number) {
