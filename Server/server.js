79c9eadafafb94372836f846bfece0d7173ce507
raddar4 <darioncassel@Darions-MacBook-Pro.local>
2015-02-28 13:02:29 -0500

    Modifying server

--- Server/server.js
+++ Server/server.js
@@ -1,23 +1,35 @@
 if (Meteor.isServer) {
 
+  var accountSid = 'AC1b1073abee0a6a981c5b364196016cdb';
+  var authToken = '87884be75ea60edbb8fa1d0a8081020f';
+  twilio = Twilio(accountSid, authToken);
+
   Meteor.methods({
     sendSMS: function () {
-
-      twilio = Twilio(accountSid, authToken);
-        twilio.sendSms({
-          to: "7324477268",
-	        from: "+17324225154",
-	        body: "Welcome to inforum",
-        }, function(err, responseData) { //this function is executed when a response is received from Twilio
-          if (!err) {
-              console.log(responseData.from); // outputs "+14506667788"
-              console.log(responseData.body); // outputs "word to your mother."
-          }
+      twilio.sendSms({
+        to: "7324477268",
+        from: "+17324225154",
+        body: "Welcome to inforum",
+      }, function(err, responseData) {
+        if (!err) {
+          console.log(responseData.from);
+          console.log(responseData.body);
+        }else {
+          console.log(err);
+        }
       });
     }
   });
 
   Meteor.startup(function () {
-    // code to run on server at startup
+    /*
+    Meteor.setInterval(function(){
+      twilio.messages.list(function(err, data) {
+        data.messages.forEach(function(message) {
+            console.log(message.body);
+        });
+      }, 100000);
+    });
+    */
   });
 }
