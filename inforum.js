c4c4e360e269281983811502b7491b0eff91a61a
John Mourad <jmm5kg@virginia.edu>
2015-02-28 10:13:11 -0500

    Hey

--- inforum.js
+++ inforum.js
@@ -11,7 +11,7 @@ if (Meteor.isClient) {
   Template.hello.events({
     'click button': function () {
       // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 3);
+      Session.set('counter', Session.get('counter') + 1);
     }
   });
 }
