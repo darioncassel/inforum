39e7f4387687d3b747f1fcc9e8b211fe7f6f6706
raddar4 <darioncassel@Darions-MacBook-Pro.local>
2015-02-28 20:46:16 -0500

    Adding chat

--- Client/main.js
+++ Client/main.js
@@ -2,10 +2,11 @@ Router.configure({
 	layoutTemplate: 'ApplicationLayout'
 });
 
-Router.route('/', function(){this.render('Main');});
-Router.route('/:id', function() {
-  this.render('Main');
+Router.route('/', function(){this.render('main');});
+Router.route('/chat/:id', function() {
+  this.render('chat');
   var id = this.params.id;
+  Session.set('chapp-docid', id);
 });
 
 if (Meteor.isClient) {
