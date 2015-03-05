a38944c38a615b04db88b4cd51585385bd3b8d9f
Wyliew <wylie.w.wang@gmail.com>
2015-03-05 15:29:12 -0500

    Testing 60 km

--- Server/server.js
+++ Server/server.js
@@ -152,7 +152,7 @@ if (Meteor.isServer) {
             var thisGroup = StudyGroups.find({
               loc: {
                 $geoWithin: {
-                  $center: [loc, 0.02699784*2] //6000 meters
+                  $center: [loc, 0.02699784*20] //6000 meters
                 }
               }
             }).fetch()[0];
